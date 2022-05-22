3a1ee60845d1480873a6d05194724136ec63a287
Sagar <57942799+poudel-sagar@users.noreply.github.com>
2022-05-22 16:12:11 +0545

    Update CommentsActivity.java

--- app/src/main/java/com/rishav/instaclone/CommentsActivity.java
+++ app/src/main/java/com/rishav/instaclone/CommentsActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+//imports
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
